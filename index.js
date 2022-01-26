dc2597a0c2dad651f7355a1faf0cb64249f6d4e8
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-01-25 22:42:06 -0600

    renamed server.js to index.js and added const db.

---
+++ index.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'company_db'
+});

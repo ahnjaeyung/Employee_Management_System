ba5de3e718f9552c778affc0a0cf12b5eb723512
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-01-26 16:59:08 -0600

    made slight changes to schema.sql and seeds.sql. Added prompts in index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
-INSERT INTO department (id, name)
-VALUES (1, "Corporate"),
-(2, "Human Resources"),
-(3, "Accounting"),
-(4, "Engineering");
+INSERT INTO department (name)
+VALUES ("Corporate"),
+("Human Resources"),
+("Accounting"),
+("Engineering");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (10, "CEO", 1000000.00, 1),
-(11, "CTO", 900000.00, 1),
-(12, "CFO", 800000.00, 1),
-(20, "HR Manager", 120000.00, 2),
-(30, "Accountant", 90000.00, 3),
-(40, "Senior Engineer", 120000.00, 4),
-(41, "Junior Engineer", 80000.00, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 1000000.00, 1),
+("CTO", 900000.00, 1),
+("CFO", 800000.00, 1),
+("HR Manager", 120000.00, 2),
+("Accountant", 90000.00, 3),
+("Senior Engineer", 120000.00, 4),
+("Junior Engineer", 80000.00, 4);
 
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (101, "Jae", "Ahn", 10, NULL),
-(111, "Richard", "Hendricks", 11, 101),
-(121, "Erlich", "Bachman", 12, 101),
-(201, "Monica", "Hall", 20, 101),
-(301, "Jared", "Dunn", 30, 121),
-(401, "Bertram", "Gilfoyle", 40, 111),
-(411, "Dinesh", "Chugtai", 41, 401);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jae", "Ahn", 1, NULL),
+("Richard", "Hendricks", 2, 1),
+("Erlich", "Bachman", 3, 1),
+("Monica", "Hall", 4, 1),
+("Jared", "Dunn", 5, 3),
+("Bertram", "Gilfoyle", 6, 2),
+("Dinesh", "Chugtai", 7, 6);
--- index.js
+++ index.js
@@ -7,3 +7,85 @@ const db = mysql.createConnection({
     user: 'root',
     database: 'company_db'
 });
+
+const mainMenu = [
+    {
+        type: 'list',
+        message: 'Please select one of the following options.',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
+        name: 'menuOption'
+    }
+]
+
+const addDeptPrompt = [
+    {
+        type: 'input',
+        message: 'Please enter the name of the department.',
+        name: 'deptName'
+    }
+]
+
+const addRolePrompt = [
+    {
+        type: 'input',
+        message: 'Please enter the name of the role.',
+        name: 'roleName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the salary for the role.',
+        name: 'roleSalary'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the department of the role',
+        name: 'roleDept'
+    }
+]
+
+const addEmpPrompt = [
+    {
+        type: 'input',
+        message: 'Please enter the first name of the employee.',
+        name: 'EmpFirstName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the last name of the employee.',
+        name: 'EmpLastName'
+    },
+    {
+        type: 'list',
+        message: 'Please select the role of the employee.',
+        choices: ['role1', 'role2', 'role3'],
+        name: 'EmpRole'
+    },
+    {
+        type: 'list',
+        message: 'Please select the manager that the employee reports to.',
+        choices: ['mngr1', 'mngr2', 'mngr3'],
+        name: 'empMngr'
+    }
+]
+
+const updateEmpRole = [
+    {
+        type: 'list',
+        message: 'Please select an employee to update their role.',
+        choices: ['emp1', 'emp2', 'emp3'],
+        name: 'empToUpdate'
+    },
+    {
+        type: 'list',
+        message: 'Please select the new role for the employee',
+        choices: ['role1', 'role2', 'role3']
+    }
+]
+
+function init () {
+    console.log('Employee Management System');
+}
+
+
+
+init();
